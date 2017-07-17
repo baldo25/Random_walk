df6b74fcf81b839410b2a8523550133e262aed0c
Baldo25 <baldobe2000@yahoo.de>
2017-07-17 15:11:08 +0200

    updated variable values

--- variables.py
+++ variables.py
@@ -4,28 +4,29 @@ This file contains all variables declare and will be imported to main file '''
 
 # Initially defining variables to 0, will be further appended.
 
+test= " This is test sentence to check if it is working "
 
 
-E = 0  # Equity
+E = 100000  # Equity
 # Capitals
-ini_cap = 0     # Initial Capital
-curr_cap = 0    # Current Capital
-invest_cap = 0  # Invested Capital
-avail_cap = 0   # Available Capital
+ini_cap = 100000     # Initial Capital
+curr_cap = 100000    # Current Capital
+invest_cap = 100000  # Invested Capital
+avail_cap = 100000   # Available Capital
 
 
 # Prices
-pe = 0      # Entry Prices
-ps1 = 0     # Stop Price 1
-ps2 = 0     # Stop Price 2
-pp1 = 0     # Profit Price 1
-pp2 = 0     # Profit price 2
-pp3 = 0     # Profit price 3
-pp4 = 0     # Profit price 4
+pe = 34.50      # Entry Prices
+ps1 = 34.00     # Stop Price 1
+ps2 = 33.50     # Stop Price 2
+pp1 = 35.50     # Profit Price 1
+pp2 = 36.50     # Profit price 2
+pp3 = 37.50     # Profit price 3
+pp4 = 40.50     # Profit price 4
 
 pstn = 0    # Position
 
-N = 0      # Entry Amount
+N = input("Enter N the Entry Amount")       # Entry Amount
 
 # Win/loss Ration = wlr%
 wlr30 = 0.3
@@ -37,15 +38,15 @@ wlr70 = 0.7
 l = 0       # Loop number range(1-2000)
 
 # Exit Percentages
-a = 0       # the percentage that is sold at Pp1
-b = 0       # the percentage that is sold at Pp2
-c = 0       # the percentage that is sold at Pp3
-d = 0       # the percentage that is sold at Pp4
-g = 0       # the percentage that is sold at Ps1
-h = 0       # the percentage that is sold at Ps2
+a = 0.25       # the percentage that is sold at Pp1
+b = 0.25       # the percentage that is sold at Pp2
+c = 0.25       # the percentage that is sold at Pp3
+d = 0.25       # the percentage that is sold at Pp4
+g = 0.4       # the percentage that is sold at Ps1
+h = 0.6       # the percentage that is sold at Ps2
 
 # Risk Percentage range(0.5,2.5)
-rsk = 0
+rsk = 0.010
 
 # Profit
 profit1 = (pp1-pe)*N*a
